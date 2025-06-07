73a7210c61c9d3d4b94a7050cf6390be84e27d48
PatrickR <96024158+PatrickRung@users.noreply.github.com>
2025-06-07 15:29:12 -0700

    removed unused code

--- Lab5_Inits.c
+++ Lab5_Inits.c
@@ -47,11 +47,6 @@ void PWM_Init(void) {
 void PWM_Change_Duty(int cycle) {
   PWM0CTL = 0x00000000; // Disable PWM0
 
-  /*
-  PWM0CMPA = 0x0000012B;  // set duty cycle for pin 1
-  PWM0CMPB = 0x00000063;  // set duty cycle for pin 2
-  PWM0CTL = 0x00000003;
-  */
   if (cycle > PWM0LOAD) {
     cycle = PWM0LOAD; // Ensure cycle does not exceed load
   } else if (cycle < 0) {
